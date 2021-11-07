8a0aaa6351641dadd20dac03ad5762ee4fa180f1
Enzo Tarrery <81967519+enzotarrery@users.noreply.github.com>
2021-11-07 12:35:07 +0100

    Add: w3c validator

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ const browserSync = require('browser-sync').create();
 const del = require('del');
 const plumber = require('gulp-plumber');
 const htmlmin = require('gulp-htmlmin');
+const w3cjs = require('gulp-w3cjs');
 const autoprefixer = require('gulp-autoprefixer');
 const cleanCSS = require('gulp-clean-css');
 const csscomb = require('gulp-csscomb');
@@ -47,8 +48,10 @@ function browserSyncDev() {
 
 // Watcher
 function watch() {
-  gulp.watch([paths.html.src, paths.css.src, paths.js.src])
-    .on('change', browserSync.reload);
+  gulp.watch([paths.html.src, paths.css.src, paths.js.src]).on('change', () => {
+    gulp.src(paths.html.src).pipe(w3cjs());
+    browserSync.reload();
+  });
 }
 
 // Distribution clearer
@@ -58,47 +61,39 @@ function clear() {
 
 // Copy and minifying of the project files (html, css, scss, js & images)
 function html() {
-  return (
-    gulp
-      .src(paths.html.src, { since: gulp.lastRun(html) })
-      .pipe(plumber())
-      .pipe(htmlmin({ collapseWhitespace: true }))
-      .pipe(gulp.dest(paths.html.dest))
-      .pipe(browserSync.stream())
-  );
+  return gulp
+    .src(paths.html.src, { since: gulp.lastRun(html) })
+    .pipe(plumber())
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(gulp.dest(paths.html.dest))
+    .pipe(browserSync.stream());
 }
 function css() {
-  return (
-    gulp
-      .src(paths.css.src, { since: gulp.lastRun(css) })
-      .pipe(plumber())
-      .pipe(csscomb())
-      .pipe(autoprefixer({ cascade: clear }))
-      .pipe(cleanCSS({ compatibility: 'ie8' }))
-      .pipe(concat('main.css'))
-      .pipe(gulp.dest(paths.css.dest))
-      .pipe(browserSync.stream())
-  );
+  return gulp
+    .src(paths.css.src, { since: gulp.lastRun(css) })
+    .pipe(plumber())
+    .pipe(csscomb())
+    .pipe(autoprefixer({ cascade: clear }))
+    .pipe(cleanCSS({ compatibility: 'ie8' }))
+    .pipe(concat('main.css'))
+    .pipe(gulp.dest(paths.css.dest))
+    .pipe(browserSync.stream());
 }
 function js() {
-  return (
-    gulp
-      .src(paths.js.src, { since: gulp.lastRun(js) })
-      .pipe(plumber())
-      .pipe(uglify())
-      .pipe(gulp.dest(paths.js.dest))
-      .pipe(browserSync.stream())
-  );
+  return gulp
+    .src(paths.js.src, { since: gulp.lastRun(js) })
+    .pipe(plumber())
+    .pipe(uglify())
+    .pipe(gulp.dest(paths.js.dest))
+    .pipe(browserSync.stream());
 }
 function images() {
-  return (
-    gulp
-      .src(paths.images.src, { since: gulp.lastRun(images) })
-      .pipe(plumber())
-      .pipe(imagemin())
-      .pipe(gulp.dest(paths.images.dest))
-      .pipe(browserSync.stream())
-  );
+  return gulp
+    .src(paths.images.src, { since: gulp.lastRun(images) })
+    .pipe(plumber())
+    .pipe(imagemin())
+    .pipe(gulp.dest(paths.images.dest))
+    .pipe(browserSync.stream());
 }
 
 // Exports
